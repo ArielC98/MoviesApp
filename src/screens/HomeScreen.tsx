e25af916e3f667c4333ea81472674cabbb9c8d0a
Ariel <ariel.calderon@epn.edu.ec>
2023-07-24 18:20:28 -0500

    Screen Navigation

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,20 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react'
+import { Button, Text, View } from 'react-native'
+
+export const HomeScreen = () => {
+
+  const navigation = useNavigation();
+
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button
+        title='Ir detalle'
+        onPress={()=>{
+          navigation.navigate('DetailScreen' as never)
+        }}
+      />
+    </View>
+  )
+}
