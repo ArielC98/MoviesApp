6158e7fd0af8369732fbd391ebe5d70b42714ad2
Ariel <ariel.calderon@epn.edu.ec>
2023-07-24 23:15:42 -0500

    TheMovieDB Typing

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,15 +2,17 @@ import { useNavigation } from '@react-navigation/native';
 import React, { useEffect } from 'react'
 import { Button, Text, View } from 'react-native'
 import movieDB from '../api/movieDB';
+import { MovieDBNowPlaying } from '../interfaces/movieInterface';
 
 export const HomeScreen = () => {
 
   useEffect(()=>{
 
-    movieDB.get('/now_playing')
+    movieDB.get<MovieDBNowPlaying>('/now_playing')
       .then(res => {
-        console.log(res.data);
+        console.log(res.data.results[0].title);
       })
+
   },[])
 
   return (
