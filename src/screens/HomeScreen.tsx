067e77654a581ba1dfe2e383dba6af0f3205778a
Ariel <ariel.calderon@epn.edu.ec>
2023-08-09 10:21:48 -0500

    Gradient Background

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,6 +5,7 @@ import { MoviePoster } from '../components/MoviePoster';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
 import { HorizontalSlider } from '../components/HorizontalSlider';
+import { GradientBackground } from '../components/GradientBackground';
 
 const { width: windowWidth } = Dimensions.get('window')
 
@@ -23,26 +24,28 @@ export const HomeScreen = () => {
   }
 
   return (
-    <ScrollView>
+    <GradientBackground>
+      <ScrollView>
 
-      <View style={{ marginTop: top + 20 }}>
-        <View
-          style={{
-            height: 430
-          }}
-        >
+        <View style={{ marginTop: top + 20 }}>
+          <View
+            style={{
+              height: 430
+            }}
+          >
 
-          <Carousel
-            data={nowPlaying} //Se puede utilizar la notacion !nowPlaying en TypeScript para asegurar al compilador que el valor de la variable no será nulo en tiempo de ejecución y se puede poner poqrue arriba ya se hace la comprobacion de que habrá datos o sino el símbolo de carga no desaparece
-            renderItem={({ item }: any) => <MoviePoster movie={item} />}
-            sliderWidth={windowWidth}
-            itemWidth={300}
-          />
+            <Carousel
+              data={nowPlaying} //Se puede utilizar la notacion !nowPlaying en TypeScript para asegurar al compilador que el valor de la variable no será nulo en tiempo de ejecución y se puede poner poqrue arriba ya se hace la comprobacion de que habrá datos o sino el símbolo de carga no desaparece
+              renderItem={({ item }: any) => <MoviePoster movie={item} />}
+              sliderWidth={windowWidth}
+              itemWidth={300}
+            />
+          </View>
+          <HorizontalSlider movies={upcoming} title='Estrenos' />
+          <HorizontalSlider movies={popular} title='Populares' />
+          <HorizontalSlider movies={topRated} title='Mejor calificación' />
         </View>
-        <HorizontalSlider movies={upcoming} title='Estrenos'/>
-        <HorizontalSlider movies={popular} title='Populares'/>
-        <HorizontalSlider movies={topRated} title='Mejor calificación'/>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </GradientBackground>
   )
 }
